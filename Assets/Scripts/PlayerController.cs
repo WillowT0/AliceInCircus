7ee53d2deb7c967e48fa83c2e9efd51a1cd1fdd2
WillowT0 <zofia1975.zw@gmail.com>
2025-10-12 18:53:04 +0200

    idle and walk

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+//na przysz�o�� - doda� unityengine.inputsytem albo nic ne b�dzie dzia�a� i zmieni� preferencje z default na visual studio 
+
+public class PlayerController : MonoBehaviour
+{
+    public float walkSpeed = 5f;
+    Vector2 moveInput;
+
+    [SerializeField]
+    private bool _isMoving = false;
+
+    public bool IsMoving
+    {
+        get
+        {
+            return _isMoving;
+        }
+        private set
+        {
+            _isMoving = value;
+            animator.SetBool("isMoving", value);
+        }
+    }
+    public bool _isFacingRight = true;
+
+    public bool IsFacingRight
+    {
+        get { return _isFacingRight; }
+        private set
+        {
+            if (_isFacingRight != value)
+            {
+                //flip
+                transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+            }
+
+            _isFacingRight = value;
+        }
+    }
+
+    Rigidbody2D rb;
+    Animator animator;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>(); //pojebie mnie, g�wno nie dzia�a�o bo tego nie by�o ToT 
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveInput.x * walkSpeed, rb.linearVelocity.y);
+    }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+
+        IsMoving = moveInput != Vector2.zero;
+
+        SetFacingDirection(moveInput);
+    }
+
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if (moveInput.x > 0 && !IsFacingRight)
+        {
+            //w prawo
+            IsFacingRight = true;
+        }
+        else if (moveInput.x < 0 && IsFacingRight)
+        {
+            IsFacingRight = false;
+            // w lewo
+        }
+
+    }
+  
+
+}
