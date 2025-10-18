6e785ba731d54efaf83910a6200b7456f54ed92e
WillowT0 <zofia1975.zw@gmail.com>
2025-10-18 16:51:14 +0200

    Skok

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,100 +2,116 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-//na przyszï¿½oï¿½ï¿½ - dodaï¿½ unityengine.inputsytem albo nic ne bï¿½dzie dziaï¿½aï¿½ i zmieniï¿½ preferencje z default na visual studio 
+//na przysz³oœæ - dodaæ unityengine.inputsytem albo nic ne bêdzie dzia³aæ i zmieniæ preferencje z default na visual studio 
 
+[RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirections))]
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement Settings")]
     public float walkSpeed = 5f;
     public float runSpeed = 8f;
-    Vector2 moveInput;
+    public float airWalkSpeed = 5f;
 
-    public float CurrentMoveSpeed
-    {
-        get
-        {
-            if (IsMoving)
-            {
-                return IsRunning ? runSpeed : walkSpeed;
-            }
-            else
-            {
-                return 0f;
-            }
-        }
-    }
+    [Header("Jump Settings")]
+    [SerializeField] private float jumpImpulse = 12f;
+    [SerializeField] private float fallMultiplier = 2.5f;
+    [SerializeField] private float lowJumpMultiplier = 2f;
 
-    [SerializeField]
-    private bool _isMoving = false;
+    private Vector2 moveInput;
+    private bool isJumpHeld;
+
+    private Rigidbody2D rb;
+    private Animator animator;
+    private TouchingDirections touchingDirections;
+
+    [Header("State Flags")]
+    [SerializeField] private bool _isMoving = false;
+    [SerializeField] private bool _isRunning = false;
+    [SerializeField] private bool _isFacingRight = true;
 
     public bool IsMoving
     {
-        get
-        {
-            return _isMoving;
-        }
+        get => _isMoving;
         private set
         {
             _isMoving = value;
             animator.SetBool("isMoving", value);
         }
     }
-    [SerializeField]
-    private bool _isRunning = false;
+
     public bool IsRunning
     {
-        get
-        {
-            return _isRunning;
-        }
+        get => _isRunning;
         set
         {
             _isRunning = value;
             animator.SetBool("isRunning", value);
         }
     }
-    public bool _isFacingRight = true;
 
     public bool IsFacingRight
     {
-        get { return _isFacingRight; }
+        get => _isFacingRight;
         private set
         {
             if (_isFacingRight != value)
             {
-                //flip
+                //w prawo
                 transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
             }
-
             _isFacingRight = value;
         }
     }
 
-    Rigidbody2D rb;
-    Animator animator;
+    public float CurrentMoveSpeed
+    {
+        get
+        {
+            if (touchingDirections.IsGrounded)
+            {
+                return IsMoving ? (IsRunning ? runSpeed : walkSpeed) : 0f;
+            }
+            else
+            {
+                return airWalkSpeed;
+            }
+        }
+    }
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>(); //pojebie mnie, gï¿½wno nie dziaï¿½aï¿½o bo tego nie byï¿½o ToT 
-
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-
+        animator = GetComponent<Animator>();
+        touchingDirections = GetComponent<TouchingDirections>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-
     }
 
     private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(moveInput.x * CurrentMoveSpeed, rb.linearVelocity.y);
+        Vector2 velocity = rb.linearVelocity;
+
+        // Gravity adjustment for better jump feel
+        if (velocity.y < 0f)
+        {
+            // Falling
+            velocity.y += Physics2D.gravity.y * (fallMultiplier - 1f) * Time.fixedDeltaTime;
+        }
+        else if (velocity.y > 0f && !isJumpHeld)
+        {
+            // Released jump early
+            velocity.y += Physics2D.gravity.y * (lowJumpMultiplier - 1f) * Time.fixedDeltaTime;
+        }
+
+        // Apply horizontal movement
+        velocity.x = moveInput.x * CurrentMoveSpeed;
+        rb.linearVelocity = velocity;
+
+        animator.SetFloat("yVelocity", rb.linearVelocity.y);
     }
+
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
@@ -109,15 +125,12 @@ public class PlayerController : MonoBehaviour
     {
         if (moveInput.x > 0 && !IsFacingRight)
         {
-            //w prawo
             IsFacingRight = true;
         }
         else if (moveInput.x < 0 && IsFacingRight)
         {
             IsFacingRight = false;
-            // w lewo
         }
-
     }
 
     public void OnRun(InputAction.CallbackContext context)
@@ -129,6 +142,20 @@ public class PlayerController : MonoBehaviour
         else if (context.canceled)
         {
             IsRunning = false;
+        }
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.started && touchingDirections.IsGrounded)
+        {
+            animator.SetTrigger("jump");
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpImpulse);
+            isJumpHeld = true;
+        }
+        else if (context.canceled)
+        {
+            isJumpHeld = false;
         }
     }
 }
