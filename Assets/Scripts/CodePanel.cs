284588dd6acf07ea219a1f3257eec31c1a746b3e
S3VY7 <svnq2004@gmail.com>
2025-11-11 15:06:06 +0100

    Sejf

---
+++ Assets/Scripts/CodePanel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CodePanel : MonoBehaviour {
+
+	[SerializeField]
+	Text codeText;
+	string codeTextValue = "9999";
+
+	// Update is called once per frame
+	void Update () {
+		codeText.text = codeTextValue;
+
+		if (codeTextValue == "1234") {
+			Cat.isSafeOpened = true;
+		}
+
+		if (codeTextValue.Length >= 4)
+			codeTextValue = "";
+	}
+
+	public void AddDigit(string digit)
+	{
+		codeTextValue += digit;
+	}
+
+}
